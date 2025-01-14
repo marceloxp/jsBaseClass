03bd4af5df12f3f5fac9cd57268300ea4d301962
Marcelo XP <marceloxp@gmail.com>
2025-01-14 18:29:45 -0300

    - Adjust Axios example.

--- sandbox/jsBaseClass/axios.js
+++ sandbox/jsBaseClass/axios.js
@@ -6,19 +6,25 @@ class ClassAxios extends JsBaseClass {
 
     async getData() {
         try {
+            this.console.info(`1️⃣ Before Get Data`);
             const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
             this.console.log(`✅ Get Data`, response.data);
         } catch (error) {
             this.console.log(`🚨 ${error.message}`, error);
+        } finally {
+            this.console.log(`2️⃣ After Get Data`);
         }
     }
 
     async getNonExistsData() {
         try {
+            this.console.info(`1️⃣ Before Get Non Exists Data`);
             const response = await axios.get('https://not-exists/json/1');
             this.console.log(response.data);
         } catch (error) {
             this.console.log(`🚨 ${error.message}`, error);
+        } finally {
+            this.console.log(`2️⃣ After Get Non Exists Data`);
         }
     }
 }
