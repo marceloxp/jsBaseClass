1c8a848986697c827096973b6fcd15017857b0df
Marcelo XP <marceloxp@gmail.com>
2025-01-14 16:27:43 -0300

    - Refactor classes in example.

---
+++ sandbox/jsBaseClass/class-1.js
@@ -0,0 +1,21 @@
+class myClass1 extends jsBaseClass {
+    constructor(silent = false) {
+        super(silent);
+        this.init();
+    }
+
+    init() {
+        const trigger_name_1 = 'my-event-from-class-1';
+        this.on(trigger_name_1, (args) => {
+            this.console.log(`Evento disparado: ${trigger_name_1}`, args.detail);
+        });
+
+        const trigger_name_2 = 'my-event-from-class-2';
+        this.on(trigger_name_2, (args) => {
+            this.console.log(`Evento disparado: ${trigger_name_2}`, args.detail);
+        });
+
+        this.trigger('my-event-from-class-1', 'Hello World!');
+    }
+}
+new myClass1();
